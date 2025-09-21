f953271aad733a5a12ad5ffd0ad1fa3b0985516f
Maaya02 <mayatejoh@gmail.com>
2025-09-21 13:58:17 +0800

    merge issues

--- SPDrive/src/App.tsx
+++ SPDrive/src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Input } from './components/ui/input'
+// import { Input } from './components/ui/input'
 
 function App() {
 
